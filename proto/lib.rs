0970c3221e858d1a7023c61d9ad0d9c951bbc2cd
Igor Zinkovsky <igor@deno.com>
2023-11-08 07:28:27 -0800

    make convert mod non-public

--- proto/lib.rs
+++ proto/lib.rs
@@ -1,13 +1,13 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 
 mod codec;
-pub mod convert;
+mod convert;
 mod interface;
 mod limits;
 mod protobuf;
 pub mod time;
-
 pub use crate::codec::decode_key;
 pub use crate::codec::encode_key;
+pub use crate::convert::ConvertError;
 pub use crate::interface::*;
 pub use crate::protobuf::datapath;

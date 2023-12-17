c4c6346648a2034a238fa9a5f5bfcc92e5ea18db
losfair <zhy20000919@hotmail.com>
2023-12-17 19:08:25 +0800

    consistency check

--- scripts/benchmark.ts
+++ scripts/benchmark.ts
@@ -1,10 +1,18 @@
 /// <reference lib="deno.unstable" />
 
+// Run with: deno run -A --unstable ./scripts/benchmark.ts --path http://127.0.0.1:4512 --read-concurrency 128 --write-concurrency 32 --size 10000 --consistency-check-interval 10
+
 import { parse } from "https://deno.land/std@0.191.0/flags/mod.ts";
 import { Semaphore } from "https://deno.land/x/semaphore@v1.1.2/semaphore.ts";
 
 const flags = parse(Deno.args, {
-  string: ["path", "read-concurrency", "write-concurrency", "size"],
+  string: [
+    "path",
+    "read-concurrency",
+    "write-concurrency",
+    "consistency-check-interval",
+    "size",
+  ],
   default: {},
 });
 
@@ -15,6 +23,9 @@ if (!flags["path"]) {
 
 const readConcurrency = parseInt(flags["read-concurrency"] ?? "");
 const writeConcurrency = parseInt(flags["write-concurrency"] ?? "");
+const consistencyCheckInterval = parseInt(
+  flags["consistency-check-interval"] ?? "0",
+);
 const size = parseInt(flags["size"] ?? "");
 
 if (!Number.isSafeInteger(readConcurrency) || readConcurrency <= 0) {
@@ -29,6 +40,14 @@ if (!Number.isSafeInteger(writeConcurrency) || writeConcurrency <= 0) {
 
 if (!Number.isSafeInteger(size) || size <= 0) {
   console.error("Invalid --size");
+  Deno.exit(1);
+}
+
+if (
+  !Number.isSafeInteger(consistencyCheckInterval) ||
+  consistencyCheckInterval < 0
+) {
+  console.error("Invalid --consistency-check-interval");
   Deno.exit(1);
 }
 
@@ -68,6 +87,8 @@ const kv = await Deno.openKv(flags.path);
 }
 
 const metrics = { reads: 0, writes: 0, conflicts: 0 };
+const readSemaphore = new Semaphore(readConcurrency);
+const writeSemaphore = new Semaphore(writeConcurrency);
 
 for (let i = 0; i < writeConcurrency; i++) {
   (async () => {
@@ -75,6 +96,8 @@ for (let i = 0; i < writeConcurrency; i++) {
       const a = Math.floor(Math.random() * size);
       const b = Math.floor(Math.random() * size);
       if (a === b) continue;
+
+      const release = await writeSemaphore.acquire();
 
       const amount = BigInt(Math.floor(Math.random() * 1000));
 
@@ -99,6 +122,7 @@ for (let i = 0; i < writeConcurrency; i++) {
         ).commit();
       metrics.writes++;
       if (!ok) metrics.conflicts++;
+      release();
     }
   })();
 }
@@ -106,19 +130,21 @@ for (let i = 0; i < writeConcurrency; i++) {
 for (let i = 0; i < readConcurrency; i++) {
   (async () => {
     while (true) {
+      const release = await readSemaphore.acquire();
       const id = Math.floor(Math.random() * size);
       const { value } = await kv.get<bigint>(["accounts", id]);
       if (typeof value !== "bigint") {
         throw new Error("Invalid value");
       }
       metrics.reads++;
+      release();
     }
   })();
 }
 
 let lastMetrics = { ...metrics };
 
-while (true) {
+for (let i = 0;; i++) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const [newReads, newWrites, newConflicts] = [
     metrics.reads - lastMetrics.reads,
@@ -136,4 +162,38 @@ while (true) {
       metrics.conflicts.toString().padStart(8, " ")
     }`,
   );
+
+  if (consistencyCheckInterval !== 0 && i % consistencyCheckInterval === 0) {
+    const callbacks = await Promise.all([
+      [readSemaphore, readConcurrency] as const,
+      [writeSemaphore, writeConcurrency] as const,
+    ].flatMap(([sem, concurrency]) =>
+      Array(concurrency).fill(0).map(() => sem.acquire())
+    ));
+
+    const startTime = Date.now();
+    const initSemaphore = new Semaphore(initConcurrency);
+    let balanceSum = 0n;
+    let absSum = 0n;
+    for (let i = 0; i < size; i++) {
+      const release = await initSemaphore.acquire();
+      (async () => {
+        const { value } = await kv.get<bigint>(["accounts", i]);
+        if (typeof value !== "bigint") {
+          throw new Error("Invalid value");
+        }
+        balanceSum += value;
+        absSum += value < 0n ? -value : value;
+        release();
+      })();
+    }
+    for (let i = 0; i < initConcurrency; i++) {
+      await initSemaphore.acquire();
+    }
+    if (balanceSum !== 0n) {
+      throw new Error(`Consistency check failed: ${balanceSum}`);
+    }
+    console.log(`Consistency check passed in ${Date.now() - startTime}ms, absSum=${absSum}`);
+    callbacks.forEach((x) => x());
+  }
 }

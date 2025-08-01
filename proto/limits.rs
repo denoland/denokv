583365bdead4c4a945e886094be515c4a9d2c18c
zanlei <zanlei@meituan.com>
2025-08-01 11:47:21 +0800

    perf: perf size

--- proto/limits.rs
+++ proto/limits.rs
@@ -1,8 +1,8 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
 
-pub const MAX_WRITE_KEY_SIZE_BYTES: usize = 2048;
+pub const MAX_WRITE_KEY_SIZE_BYTES: usize = 100 * 1024; // 100 KiB
 pub const MAX_READ_KEY_SIZE_BYTES: usize = MAX_WRITE_KEY_SIZE_BYTES + 1;
-pub const MAX_VALUE_SIZE_BYTES: usize = 65536;
+pub const MAX_VALUE_SIZE_BYTES: usize = 1024**3; // 1 GiB
 pub const MAX_READ_RANGES: usize = 10;
 pub const MAX_READ_ENTRIES: usize = 1000;
 pub const MAX_CHECKS: usize = 10;
